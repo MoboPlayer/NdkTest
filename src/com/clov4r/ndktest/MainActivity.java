2ba7cb26d100c88fcf2385a0cfb9017cddc4dc71
李广华 <yykxlgh@126.com>
2014-12-10 10:34:58 +0800

    加入标准注释

--- src/com/clov4r/ndktest/MainActivity.java
+++ src/com/clov4r/ndktest/MainActivity.java
@@ -1,14 +1,36 @@
 package com.clov4r.ndktest;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
-
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 MoboPlayer.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
 public class MainActivity extends ActionBarActivity {
 
 	@Override
@@ -26,11 +48,13 @@ public class MainActivity extends ActionBarActivity {
         String temp = h.openFFmpegAndSubtitleFileInJNI(libpath,libname,filePath,0);
         sb.append("open subtitle file :"+temp+"\n");
         for(int i=0;i<100;i++){
-        	String str = h.getSubtitleByTime(1000*i);
+        	String str = h.getSubtitleByTime(i*1000);
         	if(str!=null){
 
         		Log.e("subtitle", str==null?" null ": str);
+//        		if(!sb.toString().contains(str))
         		sb.append("time:"+i+"s subtitle:"+str+"\n");
+        	
         	}
         }
        
